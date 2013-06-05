f984ce3fb41d758737cfd22d982f64b834dbe1cb
Avi Tzurel <avi@kensodev.com>
2013-06-05 14:58:17 +0300

    Changed the worker names, counts and more

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -4,39 +4,27 @@ node[:applications].each do |app_name, data|
   case node[:instance_role]
     when "solo", "app", "app_master"
 
-      worker_name = "#{app_name}_job_runner" #safer to make this "#{app_name}_job_runner" if the environment might run multiple apps using delayed_job
-
-      # The symlink is created in /data/app_name/current/tmp/pids -> /data/app_name/shared/pids, but shared/pids doesn't seem to be?
-      directory "/data/#{app_name}/shared/pids" do
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0755
+      worker_name  = "#{app_name}_job_runner" #safer to make this "#{app_name}_job_runner" if the environment might run multiple apps using delayed_job
+      worker_count = 2
+      
+      worker_count.times do |count|
+        template "/etc/monit.d/delayed_job#{count+1}.#{app_name}.monitrc" do
+          source "dj.monitrc.erb"
+          owner "root"
+          group "root"
+          mode 0644
+          variables({
+            :app_name => app_name,
+            :user => node[:owner_name],
+            :worker_name => "#{app_name}_delayed_job#{count+1}",
+            :framework_env => node[:environment][:framework_env]
+          })
+        end
       end
-
-      template "/etc/monit.d/delayed_job_worker.#{app_name}.monitrc" do
-        source "delayed_job_worker.monitrc.erb"
-        owner user[:username]
-        group user[:username]
-        mode 0644
-        variables({
-                :app_name => app_name,
-                :user => node[:owner_name],
-                :worker_name => worker_name,
-                :framework_env => node[:environment][:framework_env]
-        })
+    
+      execute "monit reload" do
+         action :run
+         epic_fail true
       end
-
-    # Reload monit to pick up configuration changes
-    bash "monit-reload-restart" do
-      user "root"
-      code "monit reload && monit"
-
-      #kill all workers to remove any orphaned workers caused when monit spawns extra processes
-      #see https://cloud-support.engineyard.com/discussions/problems/415-monit-restart-doesnt-operate-reliably
-      code "pidof #{worker_name} | xargs --no-run-if-empty kill"
-
-      #were the above not a concern we could simply restart the job runner in the new environment
-      #code "monit restart #{worker_name}"
-    end
   end
 end
