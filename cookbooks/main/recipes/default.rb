f878e7aee17d1e7355942814e89c518aed403c08
Vadim Vasilov <vadim.v@cavsystems.com>
2015-02-12 18:15:59 +0200

    leave only recipes we use

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -10,7 +10,7 @@ include_recipe "timezone"
 
 require_recipe 'le'
 
-#include_recipe "mime_types"
+include_recipe "mime_types"
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # require_recipe "sphinx"
